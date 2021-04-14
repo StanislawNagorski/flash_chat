88b9c496eb91d9a9c4b8e550ec4356ab5d565007
stah <stanislaw.nagorski@gmail.com>
2021-04-14 21:21:07 +0200

    FireBase auth added to registration_screen.dart

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = 'register_screen';
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kInputTextFieldDecoration.copyWith(
+                    hintText: 'Enter your email')),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kInputTextFieldDecoration.copyWith(
+                    hintText: 'Enter your password')),
+            SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () async {
+                    try {
+                      var newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } on Exception catch (e) {
+                      print(e);
+                    }
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flash_chat/components/main_button.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    animation =
+        ColorTween(begin: Colors.blue, end: Colors.white).animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: controller.value * 100,
+                  ),
+                ),
+                DefaultTextStyle(
+                  style: TextStyle(
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black45),
+                  child: AnimatedTextKit(
+                    animatedTexts: [TypewriterAnimatedText('Flash Chat')],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            MainButton(
+              buttonColor: Colors.lightBlueAccent,
+              onPressRoute: LoginScreen.id,
+              text: 'Log In',
+            ),
+            MainButton(
+              buttonColor: Colors.blueAccent,
+              onPressRoute: RegistrationScreen.id,
+              text: 'Register',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
